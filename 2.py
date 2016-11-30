0844e6e996a688aa2268c2fc0fd6822cc082f5c8
Olivia Gardiner <oliv.gardiner@gmail.com>
2016-11-30 15:48:43 +0100

    Solution found for problem 2.

--- 2.py
+++ 2.py
@@ -1,3 +1,15 @@
 # Create a function that takes a filename as string parameter,
 # and counts the occurances of the letter "a", and returns it as a number.
 # It should not break if the file does not exist, just return 0.
+
+def count_letter_a(filename):
+    if type(filename) != str:
+        raise TypeError("Input is not a string.")
+    else:
+        counter = 0
+        for letter in filename:
+            if letter.lower() == "a":
+                counter += 1
+        return counter
+
+print(count_letter_a("MASvalami.txt"))
